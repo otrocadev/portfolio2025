da72284c14b32ad1870bfefcb14ecd5e74811fef
Ot Roca <otrocadev@gmail.com>
2025-08-21 23:56:36 +0200

    Refactor Education section to use dynamic data from mainEducation; enhance TitulationCard with additional details and update layout styles.

---
+++ src/data/education.ts
@@ -0,0 +1,16 @@
+export const mainEducation = [
+  {
+    titulation: 'Bachelor’s degree in Techniques for Software Development',
+    entity: 'Universitat Oberta de Catalunya (UOC)',
+    date: 'Expected graduation: 2026',
+    location: 'Online degree',
+    image: 'bachelor-degree.avif',
+  },
+  {
+    titulation: 'Bootcamp Frontend (Angular)',
+    entity: 'IT Academy Barcelona Activa',
+    date: 'Graduated: 2025',
+    location: 'Barcelona, Spain',
+    image: 'bootcamp.avif',
+  },
+]
