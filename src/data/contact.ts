2fc1ea817434e02429a82ea30539c582e249494c
Ot Roca <otrocadev@gmail.com>
2026-01-09 11:28:53 +0100

    refactor: move GitHub and Twitter links to contact data file for centralized management

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -4,6 +4,16 @@ const contact = [
     icon: 'linkedin',
     link: 'https://www.linkedin.com/in/otrocadev/',
   },
+  {
+    name: 'GitHub',
+    icon: 'github',
+    link: 'https://github.com/otrocadev',
+  },
+  {
+    name: 'X',
+    icon: 'twitter',
+    link: 'https://x.com/otrocadev',
+  },
 ]
 
 export default contact
